8916bc5b41df55440acac42b64136f53294211ac
rzzkv <rzzkv@yandex.ru>
2023-06-18 02:30:18 +0500

    s

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean condition = false;
+        if (condition == true) {
+            System.out.println("true");
+
+        } else {
+            System.out.println("false");
+        }
     }
 }
